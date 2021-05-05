bf27816178448f93b2a8050b625d63145aca5ff6
Helka Lantto <hmlantto@gmail.com>
2021-05-05 14:50:45 +0300

    Commit up to exercise 4.3

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -1,5 +1,43 @@
 const listHelper = require( '../utils/list_helper' )
 
+const listWithOneBlog = [
+  {
+    _id: '60915348a541053e60ce7293',
+    title: 'Cashmerette Blog',
+    author: 'Cashmerette',
+    url: 'https://blog.cashmerette.com/',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const biggerList = [
+  {
+    _id: '60915348a541053e60ce7293',
+    title: 'Cashmerette Blog',
+    author: 'Cashmerette',
+    url: 'https://blog.cashmerette.com/',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '6091539aa541053e60ce7294',
+    title: 'Megan Nielsen: The Blog',
+    author: 'Megan Nielsen Patterns',
+    url: 'https://blog.megannielsen.com/',
+    likes: 3,
+    __v: 0
+  },
+  {
+    _id: '609153caa541053e60ce7295',
+    title: 'Muna and Broad Blog',
+    author: 'Muna and Broad',
+    url: 'https://www.munaandbroad.com/blogs/news',
+    likes: 7,
+    __v: 0
+  }
+]
+
 test( 'dummy returns one', () => {
   const blogs = []
 
@@ -8,44 +46,6 @@ test( 'dummy returns one', () => {
 })
 
 describe( 'total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '60915348a541053e60ce7293',
-      title: 'Cashmerette Blog',
-      author: 'Cashmerette',
-      url: 'https://blog.cashmerette.com/',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const biggerList = [
-    {
-      _id: '60915348a541053e60ce7293',
-      title: 'Cashmerette Blog',
-      author: 'Cashmerette',
-      url: 'https://blog.cashmerette.com/',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '6091539aa541053e60ce7294',
-      title: 'Megan Nielsen: The Blog',
-      author: 'Megan Nielsen Patterns',
-      url: 'https://blog.megannielsen.com/',
-      likes: 3,
-      __v: 0
-    },
-    {
-      _id: '609153caa541053e60ce7295',
-      title: 'Muna and Broad Blog',
-      author: 'Muna and Broad',
-      url: 'https://www.munaandbroad.com/blogs/news',
-      likes: 7,
-      __v: 0
-    }
-  ]
-
   test( 'of empty list is zero', () => {
     expect( listHelper.totalLikes( []) ).toBe( 0 )
   })
