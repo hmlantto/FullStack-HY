8c75a475730cc4753b8face24480fe6143a7a1b3
Helka Lantto <hmlantto@gmail.com>
2021-05-06 14:52:45 +0300

    Commit up to exercise 4.8

---
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -0,0 +1,20 @@
+const mongoose = require( 'mongoose' )
+const supertest = require( 'supertest' )
+const helper = require( './test_helper' )
+const app = require( '../app' )
+const api = supertest( app )
+const Blog = require( '../models/blog' )
+
+beforeEach( async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany( helper.initialBlogs )
+})
+
+test( 'GET returns correct number of notes', async () => {
+  const response = await api.get('/api/blogs')
+  expect( response.body ).toHaveLength( helper.initialBlogs.length )
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
