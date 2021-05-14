297859b58f9e29d7142cb0a44fd6d99e29e545e5
Helka Lantto <hmlantto@gmail.com>
2021-05-14 15:27:08 +0300

    Commit up to exercise 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require( 'jsonwebtoken' )
 const blogsRouter = require( 'express' ).Router()
 const Blog = require( '../models/blog' )
 const User = require( '../models/user' )
+
+const getTokenFrom = request => {
+  const authorization = request.get( 'authorization' )
+  if ( authorization && authorization.toLowerCase().startsWith( 'bearer ' ) ) {
+    return authorization.substring( 7 )
+  }
+  return null
+}
 
 blogsRouter.get( '/', async ( request, response ) => {
   const blogs = await Blog
@@ -26,12 +35,22 @@ blogsRouter.post( '/', async ( request, response ) => {
     return response.status( 400 ).end()
   }
 
+  const token = getTokenFrom( request )
+  if ( !token ) {
+    return response.status( 401 ).json( { error: 'token missing' } )
+  }
+
+  const decodedToken = jwt.verify( token, process.env.SECRET )
+  if ( !decodedToken.id ) {
+    return response.status( 401 ).json( { error: 'token invalid' } )
+  }
+
+  const user = await User.findById( decodedToken.id )
+  blog.user = user._id
+
   if ( blog.likes === undefined ) {
     blog.likes = 0
   }
-
-  const user = await User.findOne()
-  blog.user = user._id
 
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat( savedBlog._id )
