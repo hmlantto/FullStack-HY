b2aa0d377bea2a2d88479b68fd1ff54aff1d9bdb
Helka Lantto <hmlantto@gmail.com>
2021-05-14 15:48:10 +0300

    Commit up to exercise 4.20

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -3,14 +3,6 @@ const blogsRouter = require( 'express' ).Router()
 const Blog = require( '../models/blog' )
 const User = require( '../models/user' )
 
-const getTokenFrom = request => {
-  const authorization = request.get( 'authorization' )
-  if ( authorization && authorization.toLowerCase().startsWith( 'bearer ' ) ) {
-    return authorization.substring( 7 )
-  }
-  return null
-}
-
 blogsRouter.get( '/', async ( request, response ) => {
   const blogs = await Blog
     .find({}).populate( 'user', { username: 1, name: 1, id: 1 } )
@@ -35,12 +27,7 @@ blogsRouter.post( '/', async ( request, response ) => {
     return response.status( 400 ).end()
   }
 
-  const token = getTokenFrom( request )
-  if ( !token ) {
-    return response.status( 401 ).json( { error: 'token missing' } )
-  }
-
-  const decodedToken = jwt.verify( token, process.env.SECRET )
+  const decodedToken = jwt.verify( request.token, process.env.SECRET )
   if ( !decodedToken.id ) {
     return response.status( 401 ).json( { error: 'token invalid' } )
   }
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -1,17 +1,15 @@
-const logger = require( './logger' )
+const tokenExtractor = ( request, response, next ) => {
+  const authorization = request.get( 'authorization' )
 
-const errorHandler = ( error, request, response, next ) => {
-  logger.error( error.message )
-
-  if ( error.name === 'CastError' ) {
-    return response.status( 400 ).send( { error: 'malformatted id' } )
-  } else if ( error.name === 'ValidationError' ) {
-    return response.status(400).json( { error: error.message } )
+  if ( authorization && authorization.toLowerCase().startsWith( 'bearer ' ) ) {
+    request.token = authorization.substring( 7 )
+  } else {
+    return response.status( 401 ).json( { error: 'token missing or invalid' } )
   }
 
-  next( error )
+  next()
 }
 
 module.exports = {
-  errorHandler
+  tokenExtractor
 }
