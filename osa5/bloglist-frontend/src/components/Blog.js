1b80e4859dfbc863880573d4fe2f54af6e2f49d6
Helka Lantto <hmlantto@gmail.com>
2021-06-15 11:30:27 +0300

    Commit up to exercise 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [ showAll, setShowAll ] = useState( false )
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setShowAll( !showAll )
+  }
+
+  if ( showAll === false ) {
+    return (
+      <div style={ blogStyle }>
+        { blog.title } { blog.author } <button onClick={ toggleVisibility }>view</button>
+      </div>  
+    )
+  }
+
+  return (
+    <div style={ blogStyle }>
+      { blog.title } { blog.author } <button onClick={ toggleVisibility }>hide</button><br />
+      { blog.url }<br />
+      likes { blog.likes } <button>like</button><br />
+      { blog.user.name }
+    </div>  
+  )
+}
 
 export default Blog
